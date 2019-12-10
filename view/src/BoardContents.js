28d891b8b77a0fad27685b4fb68bd86fded96671
Yuki Kakegawa <fasxyuqi@gmail.com>
2019-12-10 19:27:48 +0900

    チームのボードのフロント部分を追加

--- view/src/BoardContents.js
+++ view/src/BoardContents.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import './BoardContents.scss';
-import { findAllTasks } from './mock/TaskMock.js';
+import { findPersonalTasks, findTeamTaskss, findTeamTasks } from './mock/TaskMock.js';
 
 class BoardContents extends React.Component {
     render() {
         return (
             <div className="body">
-                <div class="board-section">
+                <div class="board-section personal-board">
                     <div class="header">
                         <div class="header__icon">▷</div>
                         <div class="header__name">パーソナルボード</div>
                     </div>
                     <ul class="section-list">
-                        {findAllTasks().map(c => <li class="section-item">{c}</li>)}
+                        {findPersonalTasks().map(c => <li class="section-item">{c}</li>)}
+                    </ul>
+                </div>
+                <div class="board-section team-board">
+                    <div class="header">
+                        <div class="header__icon">▷</div>
+                        <div class="header__name">チームボード</div>
+                    </div>
+                    <ul class="section-list">
+                        {findTeamTasks().map(c => <li class="section-item">{c}</li>)}
                     </ul>
                 </div>
             </div>
--- view/src/mock/TaskMock.js
+++ view/src/mock/TaskMock.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-var tasks = [
+var personalTasks = [
     "Javaプロジェクトのボード",
     "Kotlinプロジェクトのボード",
     "Perlプロジェクトのボード",
     "Goプロジェクトのボード",
 ]
 
-export function findAllTasks() {
-    return tasks
+var teamTasks = [
+    "ストーリーを考える",
+    "見積もりをする",
+    "テストを書く",
+    "実装をする",
+    "リリースする"
+]
+
+export function findPersonalTasks() {
+    return personalTasks
+}
+
+export function findTeamTasks() {
+    return teamTasks
 }
