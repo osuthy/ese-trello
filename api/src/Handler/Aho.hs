54e1bc0eb2d743a01aa6d526e87a8c3cc7717689
ShoichiroKitano <ohmybigcatkotarou@gmail.com>
2020-01-09 21:57:57 +0900

    jsonがいいかんじ

---
+++ api/src/Handler/Aho.hs
@@ -0,0 +1,19 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Handler.Aho where
+
+import Import
+
+data Person = Person {
+    name :: Text
+    , age  :: Int
+} deriving Show
+
+instance ToJSON Person where
+  toJSON (Person name age) =
+    object ["name" .= name, "age" .= age]
+
+
+getAhoR :: Handler Value
+getAhoR = do
+  let aho = Person { name = "aho", age = 12 }
+  jsonToRepJson $ aho
---
+++ api/test/Handler/AhoSpec.hs
@@ -0,0 +1,10 @@
+module Handler.AhoSpec (spec) where
+
+import TestImport
+
+spec :: Spec
+spec = withApp $ do
+
+    describe "getAhoR" $ do
+        error "Spec not implemented: getAhoR"
+
